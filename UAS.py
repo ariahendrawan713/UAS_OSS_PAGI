49b1b07394c49f3e820681e93927c6fe1a9a901e
Arief Fatih Naufal <48037514+Slozarion@users.noreply.github.com>
2019-06-24 15:06:44 +0700

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -2,7 +2,7 @@ def Penjumlahan():
 	hasil=500+200
 	return hasil
   
-def Penjumlahan(args):
+def Penjumlahan(*kwargs):
 	hasil=500+200
 	return hasil
   
@@ -10,16 +10,16 @@ def Pengurangan():
 	hasil=700-190
 	return hasil
   
-def Pengurangan(args):
+def Pengurangan(*kwargs):
 	hasil=700-190
 	return hasil
   
 def Tampil(func,func1):
-	print "Anto memiliki bebek "func()" ekor di Mataram"
-	print "Bebek anto pada tahun 2013 menjadi "func1()" karena mati keracunan"
+	print ("Anto memiliki bebek {} ekor di Mataram".format(func))
+	print ("Bebek anto pada tahun 2013 menjadi {} karena mati keracunan".format(func1))
 
 def Utama():
-	Tampil(Penjumlahan,Pengurangan)
+	Tampil(Penjumlahan(),Pengurangan())
 
 #pemanggilan fungsi Utama
 Utama()
