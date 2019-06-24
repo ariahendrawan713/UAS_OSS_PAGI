0d7002ab132b5a965e0a33ac09acb997a0972564
IndraSuryaBudiUtomoSasmito <49376260+IndraSuryaBudiUtomoSasmito@users.noreply.github.com>
2019-06-24 11:07:42 +0700

    indra surya B.U.S (G.211.17.0022) UAS.py

--- UAS.py
+++ UAS.py
@@ -1,22 +1,22 @@
 def Penjumlahan():
 	hasil=500+200
 	return hasil
-  
-def Penjumlahan(args):
+  print Penjumlahan()
+def Penjumlahan(*args):
 	hasil=500+200
 	return hasil
-  
+print Penjumlahan()
 def Pengurangan():
 	hasil=700-190
 	return hasil
-  
-def Pengurangan(args):
+  print Pengurangan()
+def Pengurangan(*args):
 	hasil=700-190
 	return hasil
-  
+  print Pengurangan()
 def Tampil(func,func1):
-	print "Anto memiliki bebek "func()" ekor di Mataram"
-	print "Bebek anto pada tahun 2013 menjadi "func1()" karena mati keracunan"
+	print "Anto memiliki bebek ",func()," ekor
+	print "Bebek anto pada tahun 2013 menjadi "func1()" karena mati keracunan di Mataram"
 
 def Utama():
 	Tampil(Penjumlahan,Pengurangan)
